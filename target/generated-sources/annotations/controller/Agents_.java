21ed95ec2dde2adb61e4c66a48336c44c2473a79
JonghyoLim <hyokun07@gmail.com>
2020-02-27 11:14:33 +0000

    spring-webmvc 5.2.2 to 5.2.4 pom.xml

---
+++ target/generated-sources/annotations/controller/Agents_.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-27T11:07:41")
+@StaticMetamodel(Agents.class)
+public class Agents_ { 
+
+    public static volatile SingularAttribute<Agents, Integer> agentId;
+    public static volatile SingularAttribute<Agents, String> password;
+    public static volatile SingularAttribute<Agents, String> phone;
+    public static volatile SingularAttribute<Agents, Double> averageSalesThisYear;
+    public static volatile SingularAttribute<Agents, Date> dateJoined;
+    public static volatile SingularAttribute<Agents, String> name;
+    public static volatile SingularAttribute<Agents, String> fax;
+    public static volatile SingularAttribute<Agents, String> email;
+    public static volatile SingularAttribute<Agents, String> username;
+
+}
